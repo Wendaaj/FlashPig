5438c589381beeade3cbcf9442d035c4f1dff405
Wendy <pau.vendy@gmail.com>
2020-09-14 18:41:54 +0200

    Made GameLogic an abstract class

--- app/src/main/java/com/example/flashpig/Model/GameLogic.java
+++ app/src/main/java/com/example/flashpig/Model/GameLogic.java
@@ -1,41 +1,38 @@
 package com.example.flashpig.Model;
 
 import android.media.Image;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
-public class GameLogic {
+public abstract class GameLogic extends AppCompatActivity {
     String gameTitle;
+    View vGame;
     Deck deck;
     Cards chosenCard1, chosenCard2;
     List<Cards> frontsideCards, backsideCards;
+    Button buttonHome, buttonRestart;
 
-    public GameLogic(String title, Deck deck, List<Cards> frontsideCards, List<Cards> backsideCards) {
+    public GameLogic(String title, View viewGame, Deck deck, List<Cards> frontsideCards, List<Cards> backsideCards) {
         this.gameTitle = title;
+        this.vGame = viewGame;
         this.deck = deck;
 
         for (Cards card : deck.deck) {
-            frontsideCards.add(card.getFrontside());
-            backsideCards.add(card.getFrontside());
+            //frontsideCards.add(card.getFrontside());
+            //backsideCards.add(card.getFrontside());
         }
     }
 
-    private Image gameWon(int deckSize) {
-        if (deckSize == 0) {
-            return Image;
-        }
-    }
+    private Image gameWon(int deckSize) {}
 
-    private void restartGame(GameLogic game) {
+    private void restartGame(View v) {}
 
-    }
+    private void returnHome(View v) {}
 
-    private void returnHome(GameLogic game) {
-
-    }
-
-    private void magicAlgoritm() {
-
-    }
+    private void magicAlgoritm() {}
 }
 
