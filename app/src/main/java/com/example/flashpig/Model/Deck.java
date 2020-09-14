ec8dec6590d98ea80bad6c69208cdd25f384a867
salvija <salvija.zelvyte22@gmail.com>
2020-09-14 10:36:46 +0200

    Deck class

--- app/src/main/java/com/example/flashpig/Model/Deck.java
+++ app/src/main/java/com/example/flashpig/Model/Deck.java
@@ -1,11 +1,27 @@
 package com.example.flashpig.Model;
 
-import com.example.flashpig.Cards;
-
 import java.util.List;
 
 public class Deck {
     public List<Cards> deck;
     public String deckName;
     private int amountcards;
+
+    public Deck(List<Cards> deck, String deckName, int amountcards) {
+        this.deck = deck;
+        this.deckName = deckName;
+        this.amountcards = amountcards;
+    }
+    private void addCard(Cards card){
+
+    }
+    private void deleteCard(Cards card){
+
+    }
+    private void editCard(Cards card){
+
+    }
+    private void clearDeck(List<Cards> deck){
+
+    }
 }
