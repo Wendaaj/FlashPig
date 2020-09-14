0fc87a25bbbdde9811ab8331f358574242e0217a
Wendy <pau.vendy@gmail.com>
2020-09-14 18:59:28 +0200

    Added FlashcardProgress class

---
+++ app/src/main/java/com/example/flashpig/Model/FlashcardProgress.java
@@ -0,0 +1,16 @@
+package com.example.flashpig.Model;
+
+import java.util.List;
+
+public class FlashcardProgress {
+    Deck deck;
+
+    private void showCards(List<Cards> cards) {
+
+    }
+
+    private void moveCard() {}
+
+    private void deleteCard() {}
+
+}
