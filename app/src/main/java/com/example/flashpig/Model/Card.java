c26935c564280b1d1e2d048e40afa6b90818b188
BQ <jesper.bergquist1@gmail.com>
2020-09-15 11:43:33 +0200

    More code on Cards, Variabels and Create methods.

---
+++ app/src/main/java/com/example/flashpig/Model/Card.java
@@ -0,0 +1,26 @@
+package com.example.flashpig.Model;
+
+import android.media.Image;
+
+public class Card {
+    private int id;
+    private boolean isFrontside;
+    private String frontsideStr;
+    private String backsideStr;
+    private Image frontImg;
+    private Image backImg;
+
+
+    void  CreateFrontside(String str, Image img){
+        if (str.equals(null));
+       
+
+        }
+
+    void CreateBackside(String str, Image img){
+
+    }
+
+    
+
+}
--- app/src/main/java/com/example/flashpig/Model/Cards.java
+++
@@ -1,5 +0,0 @@
-package com.example.flashpig.Model;
-
-public class Cards {
-
-}
