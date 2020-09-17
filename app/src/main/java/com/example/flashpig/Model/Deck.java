d787b818bde7cae8822646065773eab599a96f84
salvija <salvija.zelvyte22@gmail.com>
2020-09-17 15:50:01 +0200

    Fixed comments and methods in Deck

--- app/src/main/java/com/example/flashpig/Model/Deck.java
+++ app/src/main/java/com/example/flashpig/Model/Deck.java
@@ -2,36 +2,69 @@ package com.example.flashpig.Model;
 
 import java.util.List;
 
+/**
+ * Class for Deck.
+ *
+ * @author Salvija Zelvyte.
+ * @version 2020-09-17
+ */
 public class Deck {
     public List<Card> deck;
     public int deckId;
     private String deckName;
     private int amountcards;
 
+    /**
+     * Class constructor
+     * @param deckName
+     * @param deckId
+     */
     public Deck(String deckName, int deckId) {
         this.deckName = deckName;
         this.deckId = deckId;
 
     }
-    private void addCard(Card card, int deckId){
-        deck.add(card);
 
+    /**
+     * Method for adding a new card to your deck.
+     * @param card
+     * @param deckId
+     */
+    public void addCard(Card card, int deckId){
+        deck.add(card);
     }
-    private void deleteCard(Card card){
+
+    /**
+     * Method for deleting a card from your deck.
+     * @param card
+     */
+    public void deleteCard(Card card){
         if(deck.contains(card)) {
             deck.remove(card);
+        }else {
+            System.out.println("Card does't exist in deck.");
         }
-
     }
-    private void editCard(Card card){
 
-    }
-    private void clearDeck(List<Card> deck){
-        if(deck.isEmpty()){
-            //error
-        }else{
+    /**
+     * Method for deleting all cards in a deck.
+     * @param deck
+     */
+    public void clearDeck(List<Card> deck){
+        if(!deck.isEmpty()){
             deck.clear();
+        }else{
+            System.out.println("Deck is already empty.");
         }
 
     }
+
+    /**
+     * Method returns the amount of cards in your deck.
+     * @return
+     */
+    public int getAmountCards(){
+       return deck.size();
+        }
+
 }
