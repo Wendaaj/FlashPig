1e65889a648de8340caf758e34554f0ad3a2fcf9
Wendy <pau.vendy@gmail.com>
2020-09-28 14:04:49 +0200

    Made flashcard UI

---
+++ app/src/main/java/com/example/flashpig/Flashcard/FlashcardActivity.java
@@ -0,0 +1,16 @@
+package com.example.flashpig.Flashcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.flashpig.R;
+
+public class FlashcardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard);
+    }
+}
--- app/src/main/java/com/example/flashpig/Model/GameLogic.java
+++ app/src/main/java/com/example/flashpig/Model/GameLogic.java
@@ -1,5 +1,8 @@
 package com.example.flashpig.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
@@ -8,7 +11,7 @@ import java.util.List;
  * @author wendy
  * @version 2020-09-17
  */
-public abstract class GameLogic {
+public abstract class GameLogic implements Parcelable {
     String gameTitle;
     Deck deck;
     Card chosenCard1, chosenCard2;
@@ -27,4 +30,18 @@ public abstract class GameLogic {
     public boolean gameWon(Deck deck) {
         return deck.getAmountCards() == 0;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+      //  dest.writeParcelable(deck, flags);
+        dest.writeString(gameTitle);
+    }
+
+    protected GameLogic(Parcel in) {
+        gameTitle = in.readString();
+        deck = (Deck) in.readParcelable(Deck.class.getClassLoader());
+    }
+
+
+
 }
