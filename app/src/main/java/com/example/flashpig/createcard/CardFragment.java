1143bdde9b59527c2701ca66a1abaa7c96c3f5f3
BQ <jesper.bergquist1@gmail.com>
2020-09-29 16:31:03 +0200

    Added onViewCreate() to CardFragment.

--- app/src/main/java/com/example/flashpig/createcard/CardFragment.java
+++ app/src/main/java/com/example/flashpig/createcard/CardFragment.java
@@ -2,6 +2,8 @@ package com.example.flashpig.createcard;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
@@ -39,7 +41,24 @@ public class CardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.cardcreatefront, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ccToolbar = view.findViewById(R.id.toolbar6);
+        ccToolbartextview = view.findViewById(R.id.toolbarcctext);
+        ccCameraButton = view.findViewById(R.id.imageButtoncc1);
+        ccGalleryButton = view.findViewById(R.id.imageButtoncc2);
+        ccPicCardview = view.findViewById(R.id.cardViewccfront1);
+        ccTextCardview = view.findViewById(R.id.cardView3);
+        ccTextinput = view.findViewById(R.id.ccTextinputFront);
+        ccButtonfront = view.findViewById(R.id.buttonccfront);
+        ccButtonback1 = view.findViewById(R.id.buttonccback1);
+        ccButtonback2 = view.findViewById(R.id.buttonccback2);
     }
 }
