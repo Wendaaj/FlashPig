f883d8caabf1fe5bbe25d123d032253184b8c5eb
salvija <salvija.zelvyte22@gmail.com>
2020-09-30 21:58:06 +0200

    EditDeckActivity and editDeckFragment, updated editdeck.xml

--- app/src/main/java/com/example/flashpig/EditDeck/EditDeckActivity.java
+++ app/src/main/java/com/example/flashpig/EditDeck/EditDeckActivity.java
@@ -1,15 +1,40 @@
 package com.example.flashpig.EditDeck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.flashpig.R;
 
-public class EditDeckActivity extends AppCompatActivity {
+public class EditDeckActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
+
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editdeck);
+
+        // populate the RecyclerView with cards
+        String[] data = {};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.cardRecyclerView);
+
+
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new RecyclerViewAdapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+    }
+
 }
--- app/src/main/java/com/example/flashpig/EditDeck/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/flashpig/EditDeck/RecyclerViewAdapter.java
@@ -12,37 +12,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.flashpig.R;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
-    private String[] mData;
-    private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.EditCardViewHolder> {
+    private String[] data;
+    private LayoutInflater inflator;
+    private ItemClickListener itemClickListener;
 
     public RecyclerViewAdapter(Context context, String[] data) {
-        this.mData = mData;
-        this.mInflater = LayoutInflater.from(context);
+        this.data = data;
+        this.inflator = LayoutInflater.from(context);
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.card, parent, false);
-        return new ViewHolder(view);
+    public EditCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflator.inflate(R.layout.card, parent, false);
+        return new EditCardViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.TextView.setText(mData[position]);
+    public void onBindViewHolder(@NonNull EditCardViewHolder holder, int position) {
+        holder.TextView.setText(data[position]);
     }
 
     @Override
     public int getItemCount() {
-        return mData.length;
+        return data.length;
     }
     // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class EditCardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView TextView;
 
-        ViewHolder(View itemView) {
+        EditCardViewHolder(View itemView) {
             super(itemView);
             TextView = itemView.findViewById(R.id.cardTextView);
             itemView.setOnClickListener(this);
@@ -50,18 +50,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         @Override
         public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+            if (itemClickListener != null) itemClickListener.onItemClick(view, getAdapterPosition());
         }
     }
 
     // convenience method for getting data at click position
     String getItem(int id) {
-        return mData[id];
+        return data[id];
     }
 
     // allows clicks events to be caught
     void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
+        this.itemClickListener = itemClickListener;
     }
 
     // parent activity will implement this method to respond to click events
