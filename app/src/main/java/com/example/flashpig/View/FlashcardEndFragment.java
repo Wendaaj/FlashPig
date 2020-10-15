51ace0d2ae5dc00093b283b85bcb85071437be4f
Wendy <pau.vendy@gmail.com>
2020-10-16 00:10:32 +0200

    fixed error flashcardTitle

--- app/src/main/java/com/example/flashpig/View/FlashcardEndFragment.java
+++ app/src/main/java/com/example/flashpig/View/FlashcardEndFragment.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.flashpig.Model.Deck;
 import com.example.flashpig.R;
 import com.example.flashpig.ViewModel.FlashcardViewModel;
 
