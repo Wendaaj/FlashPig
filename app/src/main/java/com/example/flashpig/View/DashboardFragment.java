e9bf59b5f148c9cf8c3bc341d31f285806c40374
Wendy <pau.vendy@gmail.com>
2020-10-20 01:41:31 +0200

    fixed dashboard design
    Arrow spinner now visible. Fixed overall design to match the design in figma

--- app/src/main/java/com/example/flashpig/View/DashboardFragment.java
+++ app/src/main/java/com/example/flashpig/View/DashboardFragment.java
@@ -2,12 +2,12 @@ package com.example.flashpig.View;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +20,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.flashpig.EditDeck.DeckSpinnerAdapter;
-import com.example.flashpig.DataBase.FakeDataBase;
 import com.example.flashpig.R;
 import com.example.flashpig.Model.Deck;
 import com.example.flashpig.ViewModel.DashboardViewModel;
@@ -34,7 +33,7 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
     private DeckSpinnerAdapter spinnerAdapter;
     private Spinner deckSpinner;
     private TextView ndecks;
-    private DashboardViewModel vm;
+    private DashboardViewModel viewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,7 +43,7 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        vm = new ViewModelProvider(getActivity()).get(DashboardViewModel.class);
+        viewModel = new ViewModelProvider(getActivity()).get(DashboardViewModel.class);
         configSpinner();
         setBtnListeners(view);
     }
@@ -52,15 +51,16 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
     private void configSpinner(){
         deckSpinner = getActivity().findViewById(R.id.chooseDeckSpinner);
         ndecks = getActivity().findViewById(R.id.ndeckstext);
-        spinnerAdapter = new DeckSpinnerAdapter(getActivity(), vm.getDecks().getValue(), this);
+        spinnerAdapter = new DeckSpinnerAdapter(getActivity(), viewModel.getDecks().getValue(), this);
         deckSpinner.setAdapter(spinnerAdapter);
-        if (vm.getChosenDeck().getValue()!= null){
-            deckSpinner.setSelection(vm.getChosenDeckPos());
+        if (viewModel.getChosenDeck().getValue()!= null){
+            deckSpinner.setSelection(viewModel.getChosenDeckPos());
         }
         deckSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                vm.setChosenDeck((Deck) parent.getItemAtPosition(position));
+                viewModel.setChosenDeck((Deck) parent.getItemAtPosition(position));
+                view.setBackgroundResource(R.drawable.card_background);
                 notifySelected();
                 spinnerAdapter.setEditBtnVisibility(view);
             }
@@ -70,7 +70,7 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
             }
         });
 
-        vm.getDecks().observe(getViewLifecycleOwner(), new Observer<ArrayList<Deck>>() {
+        viewModel.getDecks().observe(getViewLifecycleOwner(), new Observer<ArrayList<Deck>>() {
             @Override
             public void onChanged(ArrayList<Deck> decks) {
                 if(decks.size()==0){
@@ -86,7 +86,7 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
         view.findViewById(R.id.flashcard1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Parcelable wrappedDeck = Parcels.wrap(vm.getChosenDeck().getValue());
+                Parcelable wrappedDeck = Parcels.wrap(viewModel.getChosenDeck().getValue());
                 Intent intent = new Intent(getActivity(), FlashcardActivity.class);
                 intent.putExtra("deck", wrappedDeck);
                 startActivity(intent);
@@ -104,9 +104,9 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
         view.findViewById(R.id.editDeckBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!vm.getDecks().getValue().isEmpty()) {
+                if (!viewModel.getDecks().getValue().isEmpty()) {
                     Bundle bundle = new Bundle();
-                    bundle.putParcelable("deck", Parcels.wrap(vm.getChosenDeck().getValue()));
+                    bundle.putParcelable("deck", Parcels.wrap(viewModel.getChosenDeck().getValue()));
                     NavHostFragment.findNavController(DashboardFragment.this)
                             .navigate(R.id.action_FirstFragment_to_editDeckFragment, bundle);
                 }else {
@@ -122,10 +122,17 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
                         .navigate(R.id.action_FirstFragment_to_createDeckFragment);
             }
         });
+
+        view.findViewById(R.id.memory1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "This part is not unlocked OINK", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void notifySelected() {
-        vm.getChosenDeck().observe(getViewLifecycleOwner(), new Observer<Deck>() {
+        viewModel.getChosenDeck().observe(getViewLifecycleOwner(), new Observer<Deck>() {
             @Override
             public void onChanged(Deck deck) {
                 Toast.makeText(getActivity(), deck.getDeckName() + " selected", Toast.LENGTH_SHORT).show();
@@ -134,7 +141,26 @@ public class DashboardFragment extends Fragment implements DeckSpinnerAdapter.On
     }
 
     @Override
-    public void onRemoveDeckBtnClick(ConstraintLayout c, TextView t1, TextView t2, int pos) {
+    public void onRemoveDeckBtnClick(ConstraintLayout c, TextView deckName, TextView amountCards, int pos) {
+        if (viewModel.getDecks().getValue().size() != 1){ //If not the last deck
+            viewModel.removeDeck(viewModel.getDecks().getValue().get(pos)); //Remove the last deck
+            spinnerAdapter.manageVisibility(c, deckName, amountCards);
+            setSpinnerSelection(pos);
+        }else {
+            spinnerAdapter.manageVisibility(c, deckName, amountCards);
+            viewModel.removeDeck(viewModel.getDecks().getValue().get(pos)); //Remove the last deck
+            spinnerAdapter.notifyDataSetChanged();
+            Toast.makeText(getActivity(), "OINK! You have no decks left!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private void setSpinnerSelection(int position){
+        if((viewModel.getDecks().getValue().size()-1)==position){//size is 1 bigger because position 0 is included
+            deckSpinner.setSelection(0);
+        }else if(position == 0){
+            deckSpinner.setSelection(position+1);
+        } else{
+            deckSpinner.setSelection(position-1);
+        }
     }
 }
